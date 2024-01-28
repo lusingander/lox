23ee6e8d01916062a1ceee614b36bb9ca8226d1a
Kyosuke Fujimoto <kyoro.f@gmail.com>
2024-01-28 18:33:25 +0900

    Fix enum

--- jlox/src/main/scala/jlox/Token.scala
+++ jlox/src/main/scala/jlox/Token.scala
@@ -9,17 +9,51 @@ case class Token(
   override def toString(): String = s"$tp $lexeme $literal"
 
 enum TokenType:
-  case
-    // 記号 1 個のトークン
-    LeftParen, RightParen, LeftBrace, RightBrace, Comma, Dot, Minus,
-    Plus, Semicolon, Slash, Star,
-    // 記号 1 個または 2 個によるトークン
-    Bang, BangEqual, Equal, EqualEqual, Greater, GreaterEqual, Less,
-    LessEqual,
-    // リテラル
-    Identifier, String, Number,
-    // キーワード
-    And, Class, Else, False, Fun, For, If, Nil, Or, Print, Return, Super, This,
-    True, Var, While,
-    // EOF
-    Eof
+  // 記号 1 個のトークン
+  case LeftParen
+  case RightParen
+  case LeftBrace
+  case RightBrace
+  case Comma
+  case Dot
+  case Minus
+  case Plus
+  case Semicolon
+  case Slash
+  case Star
+
+  // 記号 1 個または 2 個によるトークン
+  case Bang
+  case BangEqual
+  case Equal
+  case EqualEqual
+  case Greater
+  case GreaterEqual
+  case Less
+  case LessEqual
+
+  // リテラル
+  case Identifier
+  case String
+  case Number
+
+  // キーワード
+  case And
+  case Class
+  case Else
+  case False
+  case Fun
+  case For
+  case If
+  case Nil
+  case Or
+  case Print
+  case Return
+  case Super
+  case This
+  case True
+  case Var
+  case While
+
+  // EOF
+  case Eof
