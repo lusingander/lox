1b0547be5777f671dec86d2118d15b592ca01042
Kyosuke Fujimoto <kyoro.f@gmail.com>
2024-06-08 09:54:43 +0900

    Init clox

---
+++ clox/src/add.h
@@ -0,0 +1,6 @@
+#ifndef CLOX_ADD_H
+#define CLOX_ADD_H
+
+int add(int, int);
+
+#endif //CLOX_ADD_H
---
+++ clox/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "add.h"
+
+int main()
+{
+    int x = add(1, 2);
+    printf("1 + 2 = %d\n", x);
+
+    return 0;
+}
