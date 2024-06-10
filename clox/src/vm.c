dcef2763498eff55eff2fd4ea96317e5fe8ce8b0
Kyosuke Fujimoto <kyoro.f@gmail.com>
2024-06-10 16:48:42 +0900

    Parsing tokens

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -22,7 +22,20 @@ void freeVM() {
 }
 
 InterpretResult interpret(const char* source) {
-    compile(source);
+    Chunk chunk;
+    initChunk(&chunk);
+    
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+    
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+    
+    InterpretResult result = run();
+    
+    freeChunk(&chunk);
     return INTERPRET_OK;
 }
 
